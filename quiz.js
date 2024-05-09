e18dda293af3f4d6187323f75006686904523b8d
creative-account <mario.miyamoto.shigeru@gmail.com>
2024-05-09 15:04:43 +0900

    refactor: Refactor quiz.js to store level, from, and to values in localStorage

--- quiz.js
+++ quiz.js
@@ -9,31 +9,13 @@ function getMode() {
   var level = document.getElementById("level").value;
   var from = document.getElementById("from").value;
   var to = document.getElementById("to").value;
-  return [level, from, to];
+  localStorage.setItem("level", level);
+  localStorage.setItem("from", from);
+  localStorage.setItem("to", to);
 }
 
 function randBetween(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
-function generateQuestion() {
-  var [level, from, to] = getMode();
-  clearMainScreen();
-  var options = [];
-  // Create an option
-  for (var i = 0; i < 3; i++) {
-    var option = randBetween(1, level);
-    // if the option is the same as the correct answer, generate a new option
-    while (options.includes(option)) {
-      option = randBetween(1, level);
-    }
-    options.push(option);
-  }
-  var correct = options[randBetween(0, 2)] // 正しい答えのインデックスを生成
-  options.push(correct);
-  options = shuffle(options);
-  options.push(from, to);
-  return options;
 }
 
 function shuffle(array) {
@@ -47,23 +29,8 @@ function shuffle(array) {
   return array;
 }
 
-
-function displayQuestion(options) {
-  const from = options[4];
-  switch (from) {
-    case 0:
-      question = "原子番号" + options[3];
-      break;
-    case 1:
-      question = "元素記号" + options[3];
-      break;
-    case 2:
-      question = "元素名" + options[3];
-      break;
-  }
-  const to = options[5];
-
-  // 新しい要素を作成
+function createQuizFrame(question, optionTexts, to){
+    // 新しい要素を作成
   const newDiv = document.createElement("div");
   newDiv.className = "box is-size-3 has-text-centered";
   newDiv.textContent = question;
@@ -72,15 +39,12 @@ function displayQuestion(options) {
   const brElements = document.createElement("br");
 
   // オプションのテキストとIDの配列
-  const optionTexts = options.slice(0, 3);
   const optionIds = ["option1", "option2", "option3"];
 
   // columns 要素を作成
   const columnsDiv = document.createElement("div");
   columnsDiv.className = "columns";
   columnsDiv.id = "options";
-
-
 
   // オプションのボタンを生成して columns 要素に追加
   for (let i = 0; i < optionTexts.length; i++) {
@@ -105,6 +69,70 @@ function displayQuestion(options) {
   main_screen.appendChild(columnsDiv);
 }
 
+
+function generateQuestion() {
+  var level = localStorage.getItem("level");
+  var from = localStorage.getItem("from");
+  var to = localStorage.getItem("to");
+  clearMainScreen();
+  var options = [];
+  // Create an option
+  for (var i = 0; i < 3; i++) {
+    var option = randBetween(1, level);
+    // if the option is the same as the correct answer, generate a new option
+    while (options.includes(option)) {
+      option = randBetween(1, level);
+    }
+    options.push(option);
+  }
+  var correct = options[randBetween(0, 2)] // 正しい答えのインデックスを生成
+  options.push(correct);
+  options = shuffle(options);
+  return options;
+}
+
+async function displayQuestion() {
+  for (var i = 0; i < 20; i++) {
+    var options = generateQuestion();
+    var level = localStorage.getItem("level");
+    var from = localStorage.getItem("from");
+    var to = localStorage.getItem("to");
+    
+    switch  (from) {
+      case "0":
+        var question = "原子番号 " + elements_list[options[3]][from];
+        break;
+      case "1":
+        var question = "元素記号 " + elements_list[options[3]][from];
+        break;
+      case "2":
+        var question = "元素名 " + elements_list[options[3]][from];
+        break;
+      default:
+        var question = "エラーです。m9(^Д^)ﾌﾟｷﾞｬｰ"
+    }
+    
+    var optionTexts = options.slice(0, 3);
+
+    createQuizFrame(question, optionTexts, to);
+    var option1 = document.getElementById("option1");
+    var option2 = document.getElementById("option2");
+    var option3 = document.getElementById("option3");
+    
+    var user_selected = await Promise.race([
+      new Promise(resolve => option1.addEventListener("click", () => resolve(optionTexts[0]))),
+      new Promise(resolve => option2.addEventListener("click", () => resolve(optionTexts[1]))),
+      new Promise(resolve => option3.addEventListener("click", () => resolve(optionTexts[2])))
+    ]);
+    // ユーザーが選択したオプションを処理する
+    // ここでユーザーの回答に対する処理を実装
+    // 例えば、回答の正誤をチェックして結果を表示するなど
+    console.info(i);
+  }
+
+}
+
 start_button.addEventListener("click", function() {
-  displayQuestion(generateQuestion());
+  getMode();
+  displayQuestion();
 });
