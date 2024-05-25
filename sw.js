1d7cf026bd3ee4c1622872daaaee9775effabc17
creative-account <mario.miyamoto.shigeru@gmail.com>
2024-05-25 20:57:43 +0900

    refactor: Remove unused site.webmanifest file and update manifest link in index.html

---
+++ sw.js
@@ -0,0 +1,34 @@
+var CACHE_NAME = 'atom-quiz';
+var urlsToCache = [
+  'data.js',
+  'functions.js',
+  'main.js',
+  'mode_select.js',
+  'theme.js',
+  'style.css',
+  'index.html'
+];
+
+// インストール処理
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      }
+    )
+  );
+});
+
+// リソースフェッチ時のキャッシュロード処理
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then(function(response) {
+        return response || fetch(event.request);
+      }
+    )
+  );
+});
